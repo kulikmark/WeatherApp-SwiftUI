00772b2b289de5c8a1e53078ee50a9c40289d5c9
Mark Kulik <kulik_mark@mail.ru>
2022-02-01 15:11:24 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Марк Кулик on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
